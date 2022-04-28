e1245923d1bf50e7c20fd29b04e6d543b5286956
Ricardo <riicardo.ro@gmail.com>
2022-04-27 23:07:58 -0500

    progressbar, slidevideo, mas vendidos

--- assets/js/scrollReveal.js
+++ assets/js/scrollReveal.js
@@ -1,3 +1,13 @@
+
+/* ScrollReveat */
 ScrollReveal().reveal('.imagenes',{delay: 500});
 ScrollReveal().reveal('.video',{delay: 500});
 ScrollReveal().reveal('.adopta',{delay: 500});
+
+function updateProgressBar(){
+    const {scrollTop, scrollHeight} = document.documentElement;
+    const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100 + '%';
+    document.querySelector('#progress-bar').style.setProperty('--progress', scrollPercent);
+  }
+  
+  document.addEventListener('scroll', updateProgressBar);
